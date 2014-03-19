7700b1ee4a28a002807bd2479a90b1214a9bcfc3
Gonçalo Martins <gmartins@isr.uc.pt>
2014-03-19 16:22:56 +0000

    Implemented the basic functionality of zlib, using streams. This must now be properly tested.

--- zlib_wrapper.cpp
+++ zlib_wrapper.cpp
@@ -49,6 +49,7 @@
 #include <omp.h>
 
 Results deflateTest(int num_iterations, Dataset& data) {
+  Results results;
   // Time vector for compression
   std::vector<double> compression_times;
   // Time vector for decompression
@@ -57,12 +58,34 @@ Results deflateTest(int num_iterations, Dataset& data) {
   double compression_ratio;
   // Prepare data structures
   int data_length = data.bytes.size();
-  /*char* decompressed = new char[data_length];
-  char* compressed = new char[LZ4_compressBound(data_length)];
+  unsigned char* decompressed = new unsigned char[data_length];
+  unsigned char* compressed = new unsigned char[data_length];
+  z_stream compress_stream, decompress_stream;
+  compress_stream.zalloc = decompress_stream.zalloc = Z_NULL;
+  compress_stream.zfree = decompress_stream.zfree = Z_NULL;
+  compress_stream.opaque = decompress_stream.opaque = Z_NULL;
+  // Initiate deflate and inflate with default compression level
+  int deflate_return = deflateInit(&compress_stream, -1);
+  int inflate_return = inflateInit(&decompress_stream);
+  // If init goes wrong, we return an error struct
+  if (deflate_return != Z_OK || inflate_return != Z_OK) {
+    results.dataset_name = data.name;
+    results.technique_name = "DEFLATE (zlib)";
+    results.error = true;
+    return results;
+  }
+  compress_stream.avail_in = data_length;
+  compress_stream.next_in = decompressed;
+  compress_stream.avail_out = data_length;
+  compress_stream.next_out = compressed;
+  decompress_stream.avail_in = data_length;
+  decompress_stream.next_in = compressed;
+  decompress_stream.avail_out = data_length;
+  decompress_stream.next_out = decompressed;
   
   // Copy source data into decompressed buffer
   for(int i = 0; i < data_length; i++)
-    decompressed[i] = data.bytes.at(i);*/
+    decompressed[i] = data.bytes.at(i);
     
   // Run tests
   for(int i = 0; i < num_iterations; i++) {
@@ -71,7 +94,8 @@ Results deflateTest(int num_iterations, Dataset& data) {
     double init_compress = omp_get_wtime(); 
     // Test compression
     int compressed_bytes;
-    //compressed_bytes = LZ4_compress(decompressed, compressed, data_length);
+    deflate_return = deflate(&compress_stream, Z_FINISH);
+    compressed_bytes = compress_stream.total_out;
     // Get time and calculate difference
     double compress_time = omp_get_wtime();
     compression_times.push_back(compress_time - init_compress);
@@ -81,18 +105,27 @@ Results deflateTest(int num_iterations, Dataset& data) {
     double init_decompress = omp_get_wtime();
     // Test decompression
     int decompressed_bytes;
-    //decompressed_bytes = LZ4_decompress_safe(compressed, decompressed, compressed_bytes, data_length);
-    
+    inflate_return = inflate(&decompress_stream, Z_FINISH);
+    decompressed_bytes = decompress_stream.total_out;
     // Get time and calculate difference
     double decompress_time = omp_get_wtime();
     decompression_times.push_back(decompress_time - init_decompress);
     // Set the ratio on the first iteration
     if(i == 0) compression_ratio = ((double)data.bytes.size())/((double)compressed_bytes);
 
+    /// Check for errors
+    if(inflate_return != Z_STREAM_END || deflate_return != Z_STREAM_END)
+      results.error = true;
   }
+  
+  // Clean up
+  delete compressed;
+  delete decompressed;
+  deflateEnd(&compress_stream);
+  inflateEnd(&decompress_stream);
+  
 
   // Fill in results
-  Results results;
   // Name
   results.dataset_name = data.name;
   results.technique_name = "DEFLATE (zlib)";
