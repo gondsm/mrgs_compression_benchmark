de92082e123a0620279c29217b12470053690165
Gonçalo Martins <gmartins@isr.uc.pt>
2014-03-20 12:26:26 +0000

    Added code to take care of LZMA. Unfortunately, thanks to sourceforge, we're unable to wget lzma.

--- pull_dependencies.sh
+++ pull_dependencies.sh
@@ -7,3 +7,14 @@ wget http://zlib.net/zlib-1.2.8.tar.gz
 tar xf zlib-1.2.8.tar.gz
 rm zlib-1.2.8.tar.gz
 mv zlib-1.2.8/ zlib/
+
+# Pull lzma
+#http://downloads.sourceforge.net/sevenzip/lzma920.tar.bz2
+# You'll have to get the tarball for 7-zip manually from the link above.
+# leave it on the repository's root, next to the makefile, compression_test.h
+# et al. We'll take care of the rest.
+mkdir lzma
+mv lzma920.tar.bz2 lzma/
+cd lzma
+tar xf lzma920.tar.bz2
+rm lzma920.tar.bz2
