a7682267b235556b1f4287d2b9364e6eaef50ed3
Gonçalo Martins <gondsm@gmail.com>
2014-06-17 15:17:24 +0100

    A few tweaks to the python script.
    It should now run perfectly for all data, and is compatible with
    both Python 2 and 3.

--- results/read_results.py
+++ results/read_results.py
@@ -34,7 +34,7 @@ sorted_rows = sorted(rows, key=lambda row: (row[0], int(row[2])))
 #        testfile.write('\n')
 
 # Find out how many datasets were used:
-# (I'm assuming the same datasets were used for all techniques)
+# (I'm assuming the same datasets were used for all techniques, which is a reasonable assumption)
 num_datasets = 0
 for row in sorted_rows:
     if row[0] == sorted_rows[0][0]:
@@ -51,37 +51,34 @@ technique_names = [row[0] for row in sorted_rows[0:len(sorted_rows):num_datasets
 # Build vectors
 # Ratio
 ratio = []
-for i in xrange(0, len(technique_names)):
+for i in range(0, len(technique_names)):
     ratio.append([float(row[4]) for row in sorted_rows[i*num_datasets:(i+1)*num_datasets]])
 
 # Temporal efficiency
 efficiency = []
-for i in xrange(0, len(technique_names)):
+for i in range(0, len(technique_names)):
     efficiency.append([float(row[4])/(float(row[5])+float(row[6])) for row in sorted_rows[i*num_datasets:(i+1)*num_datasets]])
 
-# Mean ratio for 3 smaller datasets
+# Mean ratio for smaller datasets
 mean_ratio = []
 num_smaller_datasets = 3
-internal_counter = 0
-for i in xrange(0, len(technique_names)):
+for i in range(0, len(technique_names)):
     local_ratio = [float(row[4]) for row in sorted_rows[i*num_datasets: i*num_datasets+num_smaller_datasets]]  
     mean_ratio.append(sum(local_ratio)/len(local_ratio))
 
-# Mean temporal efficiency for 3 smaller datasets
+# Mean temporal efficiency for smaller datasets
 mean_efficiency = []
-num_smaller_datasets = 3
-internal_counter = 0
-for i in xrange(0, len(technique_names)):
+for i in range(0, len(technique_names)):
     local_efficiency = [float(row[4])/(float(row[5])+float(row[6])) for row in sorted_rows[i*num_datasets: i*num_datasets+num_smaller_datasets]]  
     mean_efficiency.append(sum(local_efficiency)/len(local_efficiency))
 
 # Plot Stuff
-markers = 'ox'
+markers = 'ox' # We will alternate between these two markers and, since colors vary, will get enough combinations to show all data.
 
 # Plot Ratio
 fig_ratio = plot.figure(figsize=[5,7])
 plot.hold(True)
-for i in xrange(0, len(ratio)):
+for i in range(0, len(ratio)):
     plot.plot(dataset_sizes, ratio[i], ''.join(['--', markers[i%len(markers)]]))
 plot.legend(technique_names, loc='upper center', fontsize='small', labelspacing=0.1, ncol=2, fancybox=True, bbox_to_anchor=(0.5, 1.07))
 plot.gca().set_xscale('log')
@@ -95,7 +92,7 @@ plot.close(fig_ratio)
 # Plot Efficiency
 fig_eff = plot.figure(figsize=[5,7])
 plot.hold(True)
-for i in xrange(0, len(efficiency)):
+for i in range(0, len(efficiency)):
     plot.plot(dataset_sizes, efficiency[i], ''.join(['--', markers[i%len(markers)]]))
 plot.legend(technique_names, loc='upper center', fontsize='small', labelspacing=0.1, ncol=2, fancybox=True, bbox_to_anchor=(0.5, 1.07))
 plot.gca().set_yscale('log')
@@ -109,24 +106,22 @@ plot.close(fig_eff)
 
 # Mean ratio for 3 smaller datasets
 fig_mean_ratio = plot.figure()
-h = plot.bar(xrange(10),mean_ratio)
+h = plot.bar(range(10),mean_ratio)
 xticks_pos = [0.65*patch.get_width() + patch.get_xy()[0] for patch in h]
-plot.xticks(xticks_pos, technique_names, rotation = 20, ha = 'right')
+plot.xticks(xticks_pos, technique_names, rotation = 30, ha = 'right', fontsize='x-large')
 plot.grid(axis = 'y')
 plot.gca().set_axisbelow(True) # So that bars are on top of grid
-#plot.tight_layout()
-plot.ylabel(r'Mean Compression Ratio')#, fontsize='small')
+plot.ylabel(r'Mean Compression Ratio', fontsize='x-large')
 plot.savefig('mean_ratio.pdf', dpi=1000, bbox_inches='tight')
 plot.close(fig_mean_ratio)
 
 # Mean efficiency for 3 smaller datasets
 fig_mean_efficiency = plot.figure()
-h = plot.bar(xrange(10),mean_efficiency)
+h = plot.bar(range(10),mean_efficiency)
 xticks_pos = [0.65*patch.get_width() + patch.get_xy()[0] for patch in h]
-plot.xticks(xticks_pos, technique_names, rotation = 20, ha = 'right')
+plot.xticks(xticks_pos, technique_names, rotation = 30, ha = 'right', fontsize='x-large')
 plot.grid(axis = 'y')
 plot.gca().set_axisbelow(True) # So that bars are on top of grid
-#plot.tight_layout()
-plot.ylabel(r'Mean Temporal Efficiency')#, fontsize='small')
+plot.ylabel(r'Mean Temporal Efficiency', fontsize='x-large')
 plot.savefig('mean_efficiency.pdf', dpi=1000, bbox_inches='tight')
 plot.close(fig_mean_efficiency)
